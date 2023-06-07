59dcce35710e61a13f47237c38b22acbd78a2fb6
emily <eare0976@uni.sydney.edu.au>
2023-06-07 16:55:19 +1000

    Changes to header with logo, and how data is represented in javascript

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # eare0976-tracker
+https://github.com/eare0976/eare0976-tracker
+
+This simple one page website collects recipe data from users as a convenient recipe tracking application. 
+
+A lot of the code has been adapted from previous tutorial classes regarding forms and javascript. Otherwise most of the code was original or somewhat sourced or influenced from online resources such as W3Schools, replit, and mozilla. 
--- public/script.js
+++ public/script.js
@@ -8,6 +8,7 @@ const button = document.querySelector("#recipeform > button");
 const tasklist = document.getElementById("recipelist");
 
 button.addEventListener("click", function(event) { 
+
   event.preventDefault();
   let names = form.elements.name.value;
   let date = (new Date()).toLocaleDateString('en-US')
@@ -16,7 +17,6 @@ button.addEventListener("click", function(event) {
   let baketimes = form.elements.baketime.value;
   let recipes = form.elements.recipe.value;
   let ingredients = form.elements.ingredient.value;
-//    console.log(tasklist);
 addTask(names, date, categorys, preptimes, baketimes, recipes, ingredients); 
 });
 
@@ -33,24 +33,38 @@ function addTask(nameInputs, createdDates, categoryInputs, preptimeInputs, baket
     ingredientInputs,
   };
   formList.push(task);
-  renderTask(task); //
+  renderTask(task); 
 };
 
 function renderTask(task) {
   let item = document.createElement("li");
-  item.innerHTML = "<p> <strong>" + "Name " + formList.length + "PrepTime: " + formList.preptimeInput + "<br> </strong>" + "BakeTime: " + formList.baketimeInput + "<br>" + "Date: " + task.createdDate + "</p>";
-
-  tasklist.appendChild(item); //
+  item.innerHTML = 
+  `<p><strong>${task.nameInputs}</strong><br><em>${task.categoryInputs}</em><br><br><b>Prep Time: </b>
+   ${task.preptimeInputs}mins<br><b>Bake Time: </b>${task.baketimeInputs}mins <br> <b>Date Added: </b>${task.createdDates} </p> <span>test</span>
+  `;
+  // item.innerHTML = "<h2> Recipe " + formList.length + "</h2> <br> <p> <strong> Name: </strong> " + task.nameInputs + " <br> PrepTime: " + task.preptimeInputs + "<br> </strong>" + "BakeTime: " + task.baketimeInputs + "<br>" + "Date: " + task.createdDates + "</p>";
+  console.log(formList);
+  tasklist.appendChild(item); 
 
   let delButton = document.createElement("button");
-  let delButtonText = document.createTextNode("delete");
+  let delButtonText = document.createTextNode("Delete"); //🗑️
   delButton.appendChild(delButtonText);
   item.appendChild(delButton);
+
+  let expandButton = document.createElement("button");
+  let expandButtonText = document.createTextNode("Expand");
+  expandButton.appendChild(expandButtonText);
+  item.appendChild(expandButton);
 
   delButton.addEventListener("click", function(event) {
     item.remove();
   });
+    //expandButton.addEventListener("click", function(event) {
+   // need to add a function here which expands the box to show full recipe and ingredient list.
 
+ 
   form.reset();
 
 }
+
+// I want to make it so the paragraph in the div "Bakery" disappears when the user adds an item to the list, and make it so it reappears once all items are deleted (e.g. array is empty)
