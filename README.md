5cef205f127e264e95d11c1b7d04bb8b176d73aa
emily <eare0976@uni.sydney.edu.au>
2023-06-09 21:59:03 +1000

    final version of code

--- README.md
+++ README.md
@@ -1,6 +1,24 @@
 # eare0976-tracker
 https://github.com/eare0976/eare0976-tracker
 
-This simple one page website collects recipe data from users as a convenient recipe tracking application. 
+This simple one page website collects recipe data from users as a convenient recipe tracking application marketed for the niche target audience of bakers. Users are greeted with a simple display which has the header, a large button and the section 'Your bakery.' In this section, paragraph text below states the bakery is empty and to click the button to add an item. When the user clicks the button a drop down form appears with 6 different user input fields. This includes name, category, prep time, bake time, recipe and ingredients. All input fields are required for a successful submission. When the user submits the drop down form automatically closes showing that the 'empty bakery' text has disappeared and their bakery item has appeared in the bakery section. 
 
-A lot of the code has been adapted from previous tutorial classes regarding forms and javascript. Otherwise most of the code was original or somewhat sourced or influenced from online resources such as W3Schools, replit, and mozilla. 
+A lot of the code has been adapted from previous tutorial classes regarding forms and javascript. Otherwise most of the code was original or somewhat sourced or influenced from online resources such as W3Schools, replit, mozilla and chatgpt. 
+
+Desktop designed for 1920x1080px and iPhone 14 Plus at 428x926px, code has been tested for smaller sizes but works best for iPhone 14 Plus size across all pages.
+
+To view the display designed for the iPhone 14 Plus, make the chrome tab as short width ways as possible and change the zoom to 125%. *This was only tested on chrome, not sure if other browsers have different min widths*
+
+There are other comments throughout index.html, script.js and style.css for more clarification on what each section does.
+
+To run:
+Put in terminal "npm run dev" and access at http://localhost:1234
+
+Some Features
+- Users can toggle open the form with a button which has an animation effect, which has 6 different fields for them to fill out. 
+- the Recipe and Ingredients are stored as arrays, which are seperated by each new line "/n" and therefore requires users to put each step on a seperate line for it to work. This instruction is included as a placeholder in the recipe and ingredient sections
+- the Recipe steps are numbered and the ingredients are unnumbered
+- The image displayed on the bakery section cooresponds to the category input
+- the form automatically toggles view off when the form is submitted correctly 
+- the text in the bakery section only appears when the task list array is empty 
+- Users can delete their recipes, data is also removed from the array and display will disappear 
--- public/script.js
+++ public/script.js
@@ -1,17 +1,17 @@
-function myFunction() {
+function myFunction() { //applys for form pop up
     var popup = document.getElementById("myPopup");
     popup.classList.toggle("show");
 }
 
 const form = document.getElementById("recipeform");
-const button = document.querySelector("#recipeform > button");
 const tasklist = document.getElementById("recipelist");
 
-button.addEventListener("click", function(event) { 
-
+form.addEventListener("submit", function(event) { 
+  var popup = document.getElementById("myPopup");
+  popup.classList.toggle("show");
   event.preventDefault();
   let names = form.elements.name.value;
-  let date = (new Date()).toLocaleDateString('en-US')
+  let date = (new Date()).toLocaleDateString('en-AU') //used in render task
   let categorys = form.elements.category.value;
   let preptimes = form.elements.preptime.value;
   let baketimes = form.elements.baketime.value;
@@ -23,6 +23,8 @@ addTask(names, date, categorys, preptimes, baketimes, recipes, ingredients);
 var formList = [];
 
 function addTask(nameInputs, createdDates, categoryInputs, preptimeInputs, baketimeInputs, recipeInputs, ingredientInputs) {
+recipeInputs = recipeInputs.split("\n"); //turns a long string in recipeInputs into an array of strings: this is split by \n or new line (when user presses enter)
+ingredientInputs = ingredientInputs.split("\n"); //same for ingredientInputs
   let task = {
     nameInputs,
     createdDates,
@@ -37,34 +39,72 @@ function addTask(nameInputs, createdDates, categoryInputs, preptimeInputs, baket
 };
 
 function renderTask(task) {
-  let item = document.createElement("li");
-  item.innerHTML = 
-  `<p><strong>${task.nameInputs}</strong><br><em>${task.categoryInputs}</em><br><br><b>Prep Time: </b>
-   ${task.preptimeInputs}mins<br><b>Bake Time: </b>${task.baketimeInputs}mins <br> <b>Date Added: </b>${task.createdDates} </p> <span>test</span>
-  `;
-  // item.innerHTML = "<h2> Recipe " + formList.length + "</h2> <br> <p> <strong> Name: </strong> " + task.nameInputs + " <br> PrepTime: " + task.preptimeInputs + "<br> </strong>" + "BakeTime: " + task.baketimeInputs + "<br>" + "Date: " + task.createdDates + "</p>";
-  console.log(formList);
-  tasklist.appendChild(item); 
+  var paragraph = document.getElementById("test"); //gets ID of the text in the bakery section
+  //Another note: when I tried to change ID of 'test' I would get an error so I kept it
 
-  let delButton = document.createElement("button");
-  let delButtonText = document.createTextNode("Delete"); //🗑️
+  if (formList.length === 1) { //checks if the form list array is equal to 1, this is used instead of a more than as then the text will toggle instead or remain hidden
+    paragraph.classList.toggle("hidden"); //this toggles the text to hidden
+  };
+  const imagePath = getImagePath(task.categoryInputs);
+
+  function getImagePath(selectedValue) { //returns an image url based on the category a user put
+    switch (selectedValue) {
+      case 'Chocolate Confectionary':
+        return 'https://img.delicious.com.au/6SGt2wE2/del/2015/10/gingerbread-brownies-14280-1.jpg';
+      case 'Pastry':
+        return 'https://www.coccorocco.com.au/catering/home4/coccoroc/public_html/news/wp-content/uploads//2017/08/Pastries-Danish.jpg';
+      case 'Quick-Bread':
+        return 'https://food.fnr.sndimg.com/content/dam/images/food/fullset/2015/5/7/1/FN_quickbreads-scones-muffins-story-opener_s4x3.jpg.rend.hgtvcom.616.462.suffix/1431052446458.jpeg';
+      case 'Cake':
+        return 'https://assets.epicurious.com/photos/5af313a832c9206db5f1a110/4:3/w_5588,h_4191,c_limit/no-recipe-required-pound-cake-30082016.jpg';
+      case 'Pie':
+        return 'https://www.groupon.com/thegist/wp-content/uploads/2021/10/Main-1.jpeg';
+      case 'Cookie':
+        return 'https://recipes.timesofindia.com/photo/83804240/83804240.jpg';
+      case 'Bread':
+        return 'https://insanelygoodrecipes.com/wp-content/uploads/2022/08/Mixed-Breads-in-Basket-and-Wooden-Cutting-Board-800x530.jpg';
+      default:
+        return '';
+    };
+  };
+  
+  let item = document.createElement("section"); //establishes element for first row of user recipe data which includes the img, prep time, bake time, name, category, created date and the delete button
+  item.innerHTML = 
+  `<img src="${imagePath}" alt="Selected Image"> <p><strong>${task.nameInputs}</strong><br><em>${task.categoryInputs}</em><br><br><b>Prep Time: </b>
+   ${task.preptimeInputs} minutes<br><b>Bake Time: </b>${task.baketimeInputs} minutes <br> <b>Date Added: </b>${task.createdDates} </p></section>`;
+  
+  let other = document.createElement("aside"); //establishes element for second row of user recipe data which includes ordered list for recipe and unordered list for ingredients
+  other.innerHTML = `<div id="container">
+  <div class="list-container">
+  <b>Steps</b><br>
+    <ol id="ordered-list" class="list">${task.recipeInputs.map(item => `<li>${item}</li>`).join('')}</ol>
+  </div>
+  <div class="list-container">
+  <b>Ingredients</b><br>
+    <ul id="unordered-list" class="list">${task.ingredientInputs.map(item => `<li>${item}</li>`).join('')}</ul>
+  </div>
+</div>
+ `;
+  console.log(formList);
+
+  let delButton = document.createElement("button"); //code for delete button
+  let delButtonText = document.createTextNode("✖"); 
   delButton.appendChild(delButtonText);
   item.appendChild(delButton);
 
-  let expandButton = document.createElement("button");
-  let expandButtonText = document.createTextNode("Expand");
-  expandButton.appendChild(expandButtonText);
-  item.appendChild(expandButton);
+  tasklist.appendChild(item); 
+  tasklist.appendChild(other);
 
-  delButton.addEventListener("click", function(event) {
-    item.remove();
+  delButton.addEventListener("click", function(event) { //code for delete button functionality
+    item.remove(); //removes first row
+    other.remove(); //removes second row
+    formList.pop(task); //removes data from array
+    if (formList.length === 0) { //checks if the array is empty
+      paragraph.classList.toggle("hidden"); //if array is empty the "empty bakery" text reappears
+    }
   });
-    //expandButton.addEventListener("click", function(event) {
-   // need to add a function here which expands the box to show full recipe and ingredient list.
-
+  console.log(task.recipeInputs.length);
  
   form.reset();
 
-}
-
-// I want to make it so the paragraph in the div "Bakery" disappears when the user adds an item to the list, and make it so it reappears once all items are deleted (e.g. array is empty)
+};
